44cf8f540a3ea8500438b6da3163646f9f143988
unknown <mitulgarg2000@gmail.com>
2020-11-25 21:25:19 +0530

    Reload and Surfing Sorted

--- theme.js
+++ theme.js
@@ -1,3 +1,8 @@
 document.getElementById('themeChange').addEventListener('click',function() {
-    document.body.classList.toggle('darkTheme');
+    let darkThemeEnabled = document.body.classList.toggle('darkTheme');
+    localStorage.setItem('dark-theme-enabled',darkThemeEnabled)
 });
+
+if (localStorage.getItem('dark-theme-enabled')) {
+    document.body.classList.add('darkTheme');
+}
