38afe00a3518f24102c30ad8fba6d99fb2ab9e6d
unknown <mitulgarg2000@gmail.com>
2020-11-26 16:04:48 +0530

    Responsive Navbar

--- theme.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('themeChange').addEventListener('click',function() {
-    let darkThemeEnabled = document.body.classList.toggle('darkTheme');
-    localStorage.setItem('dark-theme-enabled',darkThemeEnabled)
-});
-
-if (localStorage.getItem('dark-theme-enabled')) {
-    document.body.classList.add('darkTheme');
-}
