7f64f7c3f10ae068b1c7796a6d30acf7d529b155
unknown <mitulgarg2000@gmail.com>
2021-01-23 18:10:57 +0530

    More Reduction in Code

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -1,17 +1,20 @@
 var body = document.querySelector('body')
 var navbar = document.querySelector('nav')
+var icon = document.getElementById('themeChanger')
 
 // this function is called directly from HTML checkbox
 function toggleTheme() {
     if (body.classList.contains('darkTheme')) {
         body.classList.remove('darkTheme')
         localStorage.removeItem('theme','darkTheme')
+        icon.src = "images/night.png"
         navbar.classList.remove('navbar-dark','bg-dark')
         navbar.classList.add('navbar-light','bg-light')
     }
     else {
         body.classList.add('darkTheme')
         localStorage.setItem('theme','darkTheme')
+        icon.src = "images/sunny.png"
         navbar.classList.add('navbar-dark','bg-dark')
         navbar.classList.remove('navbar-light','bg-light')
     }
@@ -22,14 +25,14 @@ function toggleTheme() {
     if (localStorage.getItem('theme') == 'darkTheme') {
         body.classList.add('darkTheme')
         localStorage.setItem('theme','darkTheme')
-        document.getElementById('slider').checked = false;
+        icon.src = "images/sunny.png"
         navbar.classList.add('navbar-dark','bg-dark')
         navbar.classList.remove('navbar-light','bg-light')
     }
     else {
         body.classList.remove('darkTheme')
         localStorage.removeItem('theme','darkTheme')
-        document.getElementById('slider').checked = true;
+        icon.src = "images/night.png"
         navbar.classList.remove('navbar-dark','bg-dark')
         navbar.classList.add('navbar-light','bg-light')
     }
