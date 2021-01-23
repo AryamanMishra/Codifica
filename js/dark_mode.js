91c1ffb69806ad65c3412548ef474e9f96aab3d9
unknown <mitulgarg2000@gmail.com>
2021-01-23 18:22:56 +0530

    Reload on click fixed

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -1,38 +1,38 @@
 var body = document.querySelector('body')
-var navbar = document.querySelector('nav')
 var icon = document.getElementById('themeChanger')
+var navbar = document.querySelector('nav')
 
-// this function is called directly from HTML checkbox
+// this function gets called on every click on the moon/sun icon
 function toggleTheme() {
     if (body.classList.contains('darkTheme')) {
         body.classList.remove('darkTheme')
-        localStorage.removeItem('theme','darkTheme')
         icon.src = "images/night.png"
+        localStorage.removeItem('theme','darkTheme')
         navbar.classList.remove('navbar-dark','bg-dark')
         navbar.classList.add('navbar-light','bg-light')
     }
     else {
         body.classList.add('darkTheme')
-        localStorage.setItem('theme','darkTheme')
         icon.src = "images/sunny.png"
+        localStorage.setItem('theme','darkTheme')
         navbar.classList.add('navbar-dark','bg-dark')
         navbar.classList.remove('navbar-light','bg-light')
     }
-}
+} 
 
-// checking theme on reload and while browsing to other pages
-(function(){
+// this function executes on every reload
+(function() {
     if (localStorage.getItem('theme') == 'darkTheme') {
         body.classList.add('darkTheme')
-        localStorage.setItem('theme','darkTheme')
         icon.src = "images/sunny.png"
+        localStorage.setItem('theme','darkTheme')
         navbar.classList.add('navbar-dark','bg-dark')
         navbar.classList.remove('navbar-light','bg-light')
     }
     else {
         body.classList.remove('darkTheme')
-        localStorage.removeItem('theme','darkTheme')
         icon.src = "images/night.png"
+        localStorage.removeItem('theme','darkTheme')
         navbar.classList.remove('navbar-dark','bg-dark')
         navbar.classList.add('navbar-light','bg-light')
     }
