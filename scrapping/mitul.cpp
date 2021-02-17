60515d643109e00f90b9fe5bbe7e4e8753bb8fd0
mitul-garg <55567099+mitul-garg@users.noreply.github.com>
2021-02-17 20:47:09 +0530

    CPP File for Codeforces Problem Code

---
+++ scrapping/mitul.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    // string contest = "https://codeforces.com/contest/1478/problem/A";
+    // string problemSet = "https://codeforces.com/problemset/problem/1475/H1";
+    string code = "";
+    string problemLink;
+    cin>>problemLink;
+    for (int i=0;i<problemLink.length();i++) {
+        if (problemLink[i] >= '0' && problemLink[i] <='9') {
+            code += problemLink[i];
+        }
+        else if (problemLink[i] >= 'A' && problemLink[i] <= 'Z' ) {
+            code += problemLink[i];
+        }
+    }
+    cout<<code<<endl;
+}
