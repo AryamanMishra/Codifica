2da107a58021d03d995ec17898c5a9d7cd050b73
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2021-02-17 20:50:16 +0530

    Codeforces python script

---
+++ script.py
@@ -0,0 +1,38 @@
+import requests    # Necessary Headers
+import json
+
+URL = 'https://codeforces.com/api/user.status?handle=' # Status API
+
+user_name = input()  # Taking Username input
+
+URL += user_name
+
+input_problem = input()  # The input problem
+
+problems_submitted = []   # List of submissions containg a string concatenated by contest ID and Question Key
+
+link = requests.get(URL)  # API request
+
+Bool = False
+
+parsed = json.loads(link.text)  
+
+json.dumps(parsed, indent=4) # Parsing API
+
+
+if parsed['status'] == 'OK':
+    for i in range(len(parsed['result'])):
+        if (str(parsed['result'][i]['verdict']) == 'OK'):
+            problems_submitted.append(str(parsed['result'][i]['problem']['contestId']) + str(parsed['result'][i]['problem']['index']))
+
+    for problem in problems_submitted:
+        if (problem == input_problem):
+            print("Bravo!! you have solved this problem")
+            Bool = True
+            break
+        
+    if Bool == False:
+        print("You have not solved this problem so you cannot write editorial for it")
+else:
+    print("Invalid username")
+
