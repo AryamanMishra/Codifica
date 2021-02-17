6f10066c0a4e23fc223fdab4647626865dc59150
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2021-02-17 20:53:46 +0530

    Update script.py

--- scrapping/script.py
+++ scrapping/script.py
@@ -9,7 +9,7 @@ URL += user_name
 
 input_problem = input()  # The input problem
 
-problems_submitted = []   # List of submissions containg a string concatenated by contest ID and Question Key
+problems_solved = []   # List of solved problems containg a string concatenated by contest ID and Question Key
 
 link = requests.get(URL)  # API request
 
@@ -20,13 +20,13 @@ parsed = json.loads(link.text)
 json.dumps(parsed, indent=4) # Parsing API
 
 
-if parsed['status'] == 'OK':
+if parsed['status'] == 'OK':      # Fetching all the solved problems
     for i in range(len(parsed['result'])):
         if (str(parsed['result'][i]['verdict']) == 'OK'):
-            problems_submitted.append(str(parsed['result'][i]['problem']['contestId']) + str(parsed['result'][i]['problem']['index']))
+            problems_solved.append(str(parsed['result'][i]['problem']['contestId']) + str(parsed['result'][i]['problem']['index']))
 
-    for problem in problems_submitted:
-        if (problem == input_problem):
+    for problem in problems_solved:
+        if (problem == input_problem):   # Checking if input problem is solved by the user
             print("Bravo!! you have solved this problem")
             Bool = True
             break
